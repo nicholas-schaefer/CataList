3b8795dead4722afc238bd9c233c51bcc9475847
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-09-15 17:10:52 -0400

    still figuring out posts and title tags

--- app.rb
+++ app.rb
@@ -1,10 +1,34 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
+helpers do
+  def page_title_tag(title="", delimiter="-")
+    return APP_TITLE if title.empty?
+    "#{APP_NAME} #{delimiter} #{title}"
+  end
+end
+
+before do
+  @app_name = "cat contacts"
+  @title ||= ""
+end
+
+
 get '/' do
-  'Hello World'
+  @page_title_tag = page_title_tag
+  # '<strong>Hello World</strong>'
+  erb :index, :layout => :layout
 end
 
 get '/contact/:slug' do
-  'CONTACT'
+  erb :contact, :layout => :layout
+end
+
+# https://www.reddit.com/r/webdev/comments/194uuru/best_structure_for_api_urls/
+post '/contacts/new' do
+  erb :index, :layout => :layout
+end
+
+post '/contacts/contact_id/{contact_id}/edit' do
+  # erb "hit!"
 end
